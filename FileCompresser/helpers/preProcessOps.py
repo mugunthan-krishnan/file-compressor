e1c4b06398166b4014a76903dc1fd6b2bf6e0ee9
Mugunthan Krishnan <mk2246@njit.edu>
2023-12-05 19:44:58 -0500

    Clear Heroku tmp folder test

--- FileCompresser/helpers/preProcessOps.py
+++ FileCompresser/helpers/preProcessOps.py
@@ -11,8 +11,25 @@ def getDatabase():
     database = connection['Cluster0']
     return database
 
+def empty_tmp_folder():
+    import os
+    import shutil
+    tmp_folder = '/tmp'
+
+    # Iterate over files in /tmp and remove them
+    for filename in os.listdir(tmp_folder):
+        file_path = os.path.join(tmp_folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Error while deleting {file_path}: {e}")
+
 def preProcessOps():
     import gridfs
+    empty_tmp_folder()
     database = getDatabase()
     fs = gridfs.GridFS(database)
     for i in fs.find():
